d38af259d55cd861af56cecc305e9dfe4559a5fd
tiagopancas <brotherj10@gmail.com>
2026-02-10 16:46:58 +0000

    Desenvolvimento da estrutura base backend das leads
    Desenvolvimento do pojo, dto e beans, das Leads.

--- src/main/java/pt/uc/dei/proj2/pojo/LeadsPojo.java
+++ src/main/java/pt/uc/dei/proj2/pojo/LeadsPojo.java
@@ -1,4 +1,65 @@
 package pt.uc.dei.proj2.pojo;
 
 public class LeadsPojo {
+
+    private int id;
+    private String title;
+    private String description;
+    private String state;
+    private String date;
+
+    //Construtor vazio para criar objetos dinamicamente e serializar/deserializar JSON
+    public LeadsPojo(){
+
+    }
+
+    public LeadsPojo(int id, String title, String description, String state, String date) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.state = state;
+        this.date = date;
+    }
+
+    //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
