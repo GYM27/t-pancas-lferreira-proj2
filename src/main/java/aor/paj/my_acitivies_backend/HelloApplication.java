7ff21f594259afa2bc93b9e23599f58aa2518d0f
GYM27 <oluis9@gmail.com>
2026-02-10 12:14:16 +0000

    Initial commit do backend

---
+++ src/main/java/aor/paj/my_acitivies_backend/HelloApplication.java
@@ -0,0 +1,9 @@
+package aor.paj.my_acitivies_backend;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
